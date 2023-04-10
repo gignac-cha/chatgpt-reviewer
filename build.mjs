9f03c84575650c3501b4ab77b3fe7d3d4981322a
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2023-04-11 05:02:10 +0900

    fix build script

--- build.mjs
+++ build.mjs
@@ -2,7 +2,7 @@ import { build } from 'esbuild';
 
 build({
   entryPoints: ['src/server.ts'],
-  outfile: 'server.js',
+  outfile: 'public/server.js',
   platform: 'node',
   target: 'node18',
   bundle: true,
